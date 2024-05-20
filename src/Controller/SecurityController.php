a568fe6cfef2c54f4a3950439b0f0f92617c04d7
hadidi09 <cheikhhadramy@yahoo.fr>
2024-05-20 12:17:54 +0200

    connexion page work

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,14 +15,16 @@ class SecurityController extends AbstractController
 
 
     // }
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    #[Route(path: '/connexion', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils, UserRepository $userRepository): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
+        $user = $userRepository->findOneBy(['email' => $lastUsername]);
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
